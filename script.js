15f9474080ab7e4f555b44dcbe52e1822c1c7eaf
deices <90718393+deices@users.noreply.github.com>
2021-11-22 16:33:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log ("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+// Variable name conventions
+console.log(firstName);
+
+let PI = 3.1415;
+let myFirstJob = "Coder";
+let myCurentJob = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+// Data types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+console.log(typeof null);
